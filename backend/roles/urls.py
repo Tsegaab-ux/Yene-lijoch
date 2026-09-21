199c7e94a17f7ce0d5036d9af2bc5243cbf2a151
Tsegaab Alemu <151125916+Tsegaab-ux@users.noreply.github.com>
2026-09-21 12:53:38 +0300

    Delete backend directory

--- backend/roles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('role/', views.RoleView.as_view(), name='viewRoles'),
-    path('role/<int:pk>/', views.RoleDetailView.as_view(), name='RoleDetail'),
-]
